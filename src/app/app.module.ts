d82421b26f2b5d2d20d0b0119098c1ba756c4aa6
ugiriwabo <ugiraconsole@gmail.com>
2019-02-05 11:30:08 +0200

    edit icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormsComponent } from './search-forms/search-forms.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
+    // FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Search} from '../search-Class/search';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  search:Search;
+  constructor(private http:HttpClient) { 
+  }
+  
+  ngOnInit() {
+    interface ApiResponse{
+      avatar_url:string;
+      name:string;
+      public_repos:number;
+      followers:number;
+      following:number;
+      html_url:string
+    }
+    this.http.get<ApiResponse>("'https://api.github.com/users/daneden?access_token4ffdfc5f0f8eed2003c5a2d95e5a4f9b01d39f08").subscribe(data=>{
+    this.search= new Search(data.avatar_url, data.name, data.public_repos, data.followers, data.following, data.html_url)
+      
+  })
+  }
+
+}
